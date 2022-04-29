2ae6a114950f52f34fded3c021315e0227156e6d
JulyAnAr88 <july.an.ar88@gmail.com>
2022-04-29 16:20:14 -0300

    arrancanding

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application } from 'pixi.js'
+
+import { Keyboard } from './utils/Keyboard';
+
+export const WIDTH = 1920;
+export const HEIGHT = 1080;
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: WIDTH,
+	height: HEIGHT
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+Keyboard.initialize();
 
-clampy.anchor.set(0.5);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
-
-app.stage.addChild(clampy);
